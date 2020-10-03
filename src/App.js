7ae440ccf13a396d6cc2573406659cbd09d16384
matthew <matthewmccowan@yahoo.com>
2020-10-03 15:51:58 -0500

    API Call, Finished Header, Setup Styles, Dropdown Selectivity works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import React from 'react';
-
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  useEffect(() => {
+    // Create a function
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
+
   return (
     <div className="">
-      <main className='text-red-500'>Working Project</main>
+      <header className="flex justify-between mb-5">
+        <h1 className="text-4xl">Covid-19 Tracker</h1>
+        <form className="">
+          <select onChange={onCountryChange} className="shadow-md h-full border-none outline-none text-xl p-2 bg-white w-full uppercase" value={country}>
+            <option value='worldwide'>WorldWide</option>
+            {countries.map((country) => (
+              <option className="bg-white" value={country.value}>
+                {country.name}
+              </option>
+            ))}
+          </select>
+        </form>
+      </header>
     </div>
   );
 }
